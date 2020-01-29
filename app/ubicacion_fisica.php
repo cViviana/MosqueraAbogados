38256e4b3fe098523a88001779eda92ec3f92866
packo <juansolar@unicauca.edu.co>
2020-01-29 14:57:07 -0500

    Correccion de variable

--- app/ubicacion_fisica.php
+++ app/ubicacion_fisica.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ubicacion_fisica extends Model
 {
-    protected $table='ubicaion_fisica';
+    protected $table='ubicacion_fisica';
     protected $fillable=['numArchivero','numGabeta'];
     
 }
