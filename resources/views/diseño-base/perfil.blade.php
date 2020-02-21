c85b682db09f8e4cc237447c4dfc3846520dbe33
Mariapaula1609 <mparistizabal@unicauca.edu.co>
2020-02-21 11:41:11 -0500

    redireccion cuano se logea

--- resources/views/diseño-base/perfil.blade.php
+++ resources/views/diseño-base/perfil.blade.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('https://use.fontawesome.com/releases/v5.3.1/css/all.css')}}" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style-admin.css') }}">
-    
+
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
@@ -21,12 +21,12 @@
 <body class="body_admin">
 <div class="nav-side-menu" id="menuPrincipal">
         <div class="brand titulo_menu">
-            MOSQUERA ABOGADOS    
+            MOSQUERA ABOGADOS
         </div>
         <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
         <div class="menu-list">
             <ul id="menu-content" class="menu-content collapse out">
-                
+
                 <li  data-toggle="collapse" data-target="#perfil" class="collapsed active">
                 <a href="#"><i class="fa fa-user"></i> Perfil<span class="arrow"></span></a>
                 </li>
@@ -41,7 +41,7 @@
                 <ul class="sub-menu collapse" id="casos">
                     <li class="active"><a href="#">Registrar Caso</a></li>
                     <li><a href="#">Listar Casos</a></li>
-                    
+
                     <li  data-toggle="collapse" data-target="#documentos" class="collapsed">
                         <a href="#">Documentos<span class="arrow"></span></a>
                     </li>
@@ -54,14 +54,14 @@
 
                 <li data-toggle="collapse" data-target="#clientes" class="collapsed">
                 <a href="#"><i class="fa fa-users"></i> Clientes <span class="arrow"></span></a>
-                </li>  
+                </li>
                     <ul class="sub-menu collapse" id="clientes">
                     <li>Registrar Cliente</li>
                     <li>Listar Clientes</li>
                 </ul>
                 <li data-toggle="collapse" data-target="#contraparte" class="collapsed">
                 <a href="#"><i class="fa fa-users"></i> Contrapartes <span class="arrow"></span></a>
-                </li>  
+                </li>
                     <ul class="sub-menu collapse" id="contraparte">
                     <li>Registrar Contraparte</li>
                     <li>Listar Contrapartes</li>
@@ -69,6 +69,17 @@
             </ul>
         </div>
     </div>
+		<footer>
+			<div class="sidebar-footer hidden-small text-center" align="center">
+							<button type="button" class="btn btn-dark" data-toggle="tooltip" data-placement="top" title="Logout" onclick="event.preventDefault();
+														document.getElementById('logout-form').submit();" align="center" style="width:80px; height:40px">
+								<span class="glyphicon glyphicon-off" aria-hidden="true" style="width:100%;"></span>
+							</button>
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+									@csrf
+							</form>
+						</div>
+		</footer>
     @yield('perfil')
     @yield('registrar-proceso-judicial')
     @yield('registrarCliente')
