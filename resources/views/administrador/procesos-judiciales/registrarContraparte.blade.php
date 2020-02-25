4710e6c0ca1ed9ea2808dc2959454d846ebaf3cf
victorhrendon <victorhrendon@unicauca.edu.co>
2020-02-25 17:36:27 -0500

    Actualizacion front blade registrar contraparte

--- resources/views/administrador/procesos-judiciales/registrarContraparte.blade.php
+++ resources/views/administrador/procesos-judiciales/registrarContraparte.blade.php
@@ -5,35 +5,38 @@
         <div class="texto_titulo">REGISTRAR CONTRAPARTE</div> 
         <div class="container_formulario">
             <div class="mascara">
+            <form action="" class="texto_campos" method="post">
+            {{csrf_field()}}
                 <br>
                 <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                    <input name="" class="form-control" placeholder="* Nombre Completo" type="text" required>
+                    <input name='nombre' id='nombre' class="form-control" placeholder="* Nombre Completo" type="text" required>
                 </div>
                 <br>
                 <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-list"></i></span>
-                    <select class="form-control">
+                    <select name='tipo' id='tipo' class="form-control">
                         <option selected="">* Seleccionar tipo de Persona</option>
                         <option>Persona Natural</option>
                         <option>Persona Juridica</option>
                     </select>
                     <span class="input-group-addon"><i class="fa fa-id-card"></i></span>
-                    <input name="" class="form-control" placeholder="* Numero de Identificacion" type="text" required>
+                    <input name='numero' id='numero' class="form-control" placeholder="* Numero de Identificacion" type="text" required>
                 </div>
                 <br>
                 <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-phone-square"></i></span>
-                    <input name="" class="form-control" placeholder="* Numero de telefono" type="text" required>
+                    <input name='telefono' id='telefono' class="form-control" placeholder="* Numero de telefono" type="text" required>
                 </div>
                 <br>
                 <div class="input-group">
                     <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                    <input name="" class="form-control" placeholder="* Correo Electronico" type="text" required>
+                    <input name='email' id='email' class="form-control" placeholder="* Correo Electronico" type="text" required>
                 </div>
                 <br>
                 <button type="submit" class="btn btn-primary">Registrar Contraparte</button>
                 <div class="texto_campos">Los campos con (*) son obligatorios</div> 
+                </form>
             </div>
         </div>
     </div>
