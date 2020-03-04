94a07d6c5cd78eed05f95fcdacf68603e965f3fd
Angelica Pinto <pangelica@unicauca.edu.co>
2020-03-04 14:52:49 -0500

    arreglo de menu

--- resources/views/diseño-base/perfil.blade.php
+++ resources/views/diseño-base/perfil.blade.php
@@ -26,8 +26,7 @@
                 </div>
                 <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
                 <div class="menu-list">
-                    <ul id="menu-content" class="menu-content collapse out">
-
+                    <ul id="menu-content" class="menu-content">
                         <li  data-toggle="collapse" data-target="#perfil" class="collapsed active">
                         <a href="#"><i class="fa fa-user"></i> Perfil<span class="arrow"></span></a>
                         </li>
