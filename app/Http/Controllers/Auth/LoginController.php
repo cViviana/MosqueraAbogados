8cb99f0585600e7f890587f787a34e74e9170fe7
juansolar <juansolar@unicauca.edu.co>
2020-03-26 13:34:41 -0500

    corrección de error vista usuario

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'perfil_usuario';
+    protected $redirectTo = 'home';
 
     /**
      * Create a new controller instance.
