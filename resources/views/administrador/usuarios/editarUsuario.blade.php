438bb1b8dbc6b3a70c868bd4ec95107a5a170cb8
victorhrendon <victorhrendon@unicauca.edu.co>
2020-03-26 22:22:05 -0500

    plantilla editar usuario

--- resources/views/administrador/usuarios/editarUsuario.blade.php
+++ resources/views/administrador/usuarios/editarUsuario.blade.php
@@ -7,5 +7,101 @@
 @section('titulo-formulario', "EDITAR DATOS DE UN USUARIO")
 
 @section('formulario')
-    <!-- Aquí va el formulario -->
+<br>
+<div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">ACTUALIZAR USUARIO</h6>
+        </div>
+        <div class="d-flex justify-content-center h-100">
+            <div class="card-body">
+                
+                    <p class="text-center m-0 font-weight-bold text-primary">Crear cuenta de</p>
+                    <p>
+                    <h4 class="text-center m-0 font-weight-bold text-primary">
+                        MOSQUERA 
+                        <br> 
+                        ABOGADOS
+                    </h4>
+                    <p class="divider-text">
+                        <span class="bg-light"></span>
+                    </p>
+               
+                <form method="POST" action="------">
+                    @csrf
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-id-card"></i> </span>
+                        </div>
+                        <input id="cedula" type="" placeholder="* Cedula" class="form-control @error('cedula') is-invalid @enderror" name="cedula" value="--" required autocomplete="cedula" autofocus>
+                        @error('cedula')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                        </div>
+                        <input id="nombre" type="text" placeholder="* Nombre" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="---" required autocomplete="nombre" autofocus>
+                        @error('nombre')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-phone"></i> </span>
+                        </div>
+                        <input id="telefono" type="text" placeholder="* Telefono" class="form-control @error('telefono') is-invalid @enderror" name="telefono" value="---" required autocomplete="telefono" autofocus>
+                        @error('telefono')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+                        </div>
+                        <input id="email" type="email" placeholder="* Correo" class="form-control @error('email') is-invalid @enderror" name="email" value="---" required autocomplete="email">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                        </div>
+                        <input id="password" type="password" placeholder="* Contraseña" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                        </div>
+                        <input id="password-confirm" type="password" placeholder="* Confirmar Contraseña" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-12 align-items-center">
+                            <button type="submit" class="btn btn-primary text-center ">
+                                {{ __('Acualizar Usuario') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
+               
+            </div>
+        </div>
+</div>
 @endsection
