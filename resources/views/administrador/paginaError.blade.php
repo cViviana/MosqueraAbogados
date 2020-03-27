b02ee6cf439b510e570170c854e04162fbcea5d5
victorhrendon <victorhrendon@unicauca.edu.co>
2020-03-27 16:08:25 -0500

    plantilla capagina de error

---
+++ resources/views/administrador/paginaError.blade.php
@@ -0,0 +1,28 @@
+@extends('diseño-base.plantilla-admin')
+@section('titulo','Inicio')
+
+@section('seccion')
+<div class="text-center">
+    <div class="alert alert-danger" role="alert">
+        Tu usuario no tiene los permisos para realizar esta funcion!
+    </div>
+    <div  >
+        <span>
+        <i class="fa fa-exclamation-triangle fa-6x" aria-hidden="true"></i>
+        </span>
+    </div>
+    <p class="lead text-gray-800 mb-5">
+        Pagina No Encontrada
+    </p>
+    <p class="text-gray-500 mb-0">
+        Lo sentimos con tu cuenta de usuario no es posible  realizar estas funciones
+    </p>
+    <p class="text-gray-500 mb-0">
+        Si deseas hacer uso de esta funcionalidad ingresa con otra cuenta <br>
+        que tenga los permisos necesarios.
+    </p>
+    <a href="{{route('perfil_usuario')}}">&larr; Volver al perfil de usuario</a>
+
+</div>
+
+@endsection
