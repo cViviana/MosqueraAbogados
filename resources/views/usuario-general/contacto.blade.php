fe5e30547b50792607721ca675faef4ab6e7a61b
victorhrendon <victorhrendon@unicauca.edu.co>
2020-03-28 16:09:34 -0500

    Validacion blade de contacto

--- resources/views/usuario-general/contacto.blade.php
+++ resources/views/usuario-general/contacto.blade.php
@@ -44,27 +44,31 @@
 					</div>
 				</div>
 				<div class="col-lg-9">
-					<form class="row contact_form" action="mail" method="post" id="contactForm" novalidate="novalidate">
+					<form class="row contact_form" action="mail" method="post" id="contactForm">
 						{{ csrf_field() }}
-						<div class="col-md-6">
+						<div class="col-md-12">
 							<div class="form-group">
 								<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingrese su nombre"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ingrese su nombre'">
+                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ingrese su nombre'" required>
 							</div>
 							<div class="form-group">
                                 <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese su correo"
                                 onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ingrese su correo'">
 							</div>
 							<div class="form-group">
+								<input type="text" class="form-control" id="telefono" name="telefono" placeholder="Ingresar su telefono"
+                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ingresar su telefono'" required>
+							</div>
+							<div class="form-group">
 								<input type="text" class="form-control" id="asunto" name="asunto" placeholder="Ingresar Asunto"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ingresar Asunto'">
+                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ingresar Asunto'"required>
 							</div>
 						</div>
 
-						<div class="col-md-6">
+						<div class="col-md-12">
 							<div class="form-group">
 								<textarea class="form-control" name="mensaje" id="mensaje" rows="1" placeholder="Añadir Mensaje"
-                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Añadir Mensaje'"></textarea>
+                                onfocus="this.placeholder = ''" onblur="this.placeholder = 'Añadir Mensaje'" required></textarea>
 							</div>
 						</div>
 
@@ -75,7 +79,7 @@
 				</div>
 			</div>
 			<div class="mapBox text-center"> 
-				
+				<br>
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3986.1971534974596!2d-76.60445958590995!3d2.4412833577784587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e300305f48b2eb3%3A0xa7ad8e3d77634c51!2sCra.%202%20%233-88%2C%20Popay%C3%A1n%2C%20Cauca!5e0!3m2!1ses!2sco!4v1585376736745!5m2!1ses!2sco" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 			</div>
            
